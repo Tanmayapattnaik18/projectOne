e0ca80714b0d6fbe76ad1c75fdbc9613ae91eb08
Tanmayapattnaik18 <143249913+Tanmayapattnaik18@users.noreply.github.com>
2024-12-17 21:15:19 +0530

    Delete sign.js

--- sign.js
+++
@@ -1,9 +0,0 @@
-function signup() {
-    let email = document.getElementById("semail").value;
-    let password = document.getElementById("spassword").value;
-    localStorage.setItem("email", email);
-    localStorage.setItem("password", password);
-    window.location.href = "newpage.html";
-    console.log(email, password);
-    return false;
-}
